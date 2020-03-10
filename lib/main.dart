3e8d07d4731fbc9757707e84cf272f7ce41387e1
mointhedev <52953600+moin248@users.noreply.github.com>
2020-03-10 12:59:43 +0500

    NavBar Responsiveness Done

--- lib/main.dart
+++ lib/main.dart
@@ -101,6 +101,11 @@ class MyHomePage extends StatelessWidget {
                         flex: 2,
                         child: LayoutBuilder(
                           builder: (context, size) {
+                            double navHeight = (size.maxHeight * 0.1 * 0.2 +
+                                    size.maxWidth * 0.1 * 0.8) *
+                                0.8;
+
+                            double fontSize = 9 + navHeight * 0.065;
                             return Container(
                               height: size.maxHeight,
                               width: size.maxWidth,
@@ -109,7 +114,7 @@ class MyHomePage extends StatelessWidget {
                                 children: <Widget>[
                                   Container(
                                     padding: EdgeInsets.only(left: 50),
-                                    height: size.maxHeight * 0.1,
+                                    height: navHeight,
                                     width: size.maxWidth,
                                     child: Row(
                                       children: <Widget>[
@@ -120,7 +125,8 @@ class MyHomePage extends StatelessWidget {
                                               'MEN',
                                               style: TextStyle(
                                                   fontFamily: 'Segoe UI',
-                                                  fontWeight: FontWeight.w600),
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: fontSize),
                                             ),
                                             PopupMenuButton(
                                               itemBuilder:
@@ -134,11 +140,12 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                 ),
-                                                const PopupMenuItem<String>(
+                                                PopupMenuItem<String>(
                                                   value: 'Value2',
                                                   child: Center(
                                                     child: Text(
@@ -146,7 +153,8 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                   enabled: true,
@@ -167,7 +175,8 @@ class MyHomePage extends StatelessWidget {
                                               'WOMEN',
                                               style: TextStyle(
                                                   fontFamily: 'Segoe UI',
-                                                  fontWeight: FontWeight.w600),
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: fontSize),
                                             ),
                                             PopupMenuButton(
                                               itemBuilder:
@@ -181,11 +190,12 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                 ),
-                                                const PopupMenuItem<String>(
+                                                PopupMenuItem<String>(
                                                   value: 'Value2',
                                                   child: Center(
                                                     child: Text(
@@ -193,7 +203,8 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                   enabled: true,
@@ -214,7 +225,8 @@ class MyHomePage extends StatelessWidget {
                                               'KIDS',
                                               style: TextStyle(
                                                   fontFamily: 'Segoe UI',
-                                                  fontWeight: FontWeight.w600),
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: fontSize),
                                             ),
                                             PopupMenuButton(
                                               itemBuilder:
@@ -228,11 +240,12 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                 ),
-                                                const PopupMenuItem<String>(
+                                                PopupMenuItem<String>(
                                                   value: 'Value2',
                                                   child: Center(
                                                     child: Text(
@@ -240,7 +253,8 @@ class MyHomePage extends StatelessWidget {
                                                       style: TextStyle(
                                                           fontFamily:
                                                               'Segoe UI',
-                                                          color: Colors.white),
+                                                          color: Colors.white,
+                                                          fontSize: fontSize),
                                                     ),
                                                   ),
                                                   enabled: true,
@@ -259,20 +273,42 @@ class MyHomePage extends StatelessWidget {
                                           'CUSTOMIZE',
                                           style: TextStyle(
                                               fontFamily: 'Segoe UI',
-                                              fontWeight: FontWeight.w600),
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: fontSize),
+                                        ),
+                                        Spacer(
+                                          flex: 5,
+                                        ),
+                                        Icon(
+                                          Icons.search,
+                                          size: fontSize * 1.75,
                                         ),
                                         Spacer(),
+                                        Icon(
+                                          Icons.shopping_cart,
+                                          size: fontSize * 1.75,
+                                        ),
                                         Spacer(),
-                                        Spacer(),
-                                        Spacer(),
-                                        Spacer(),
-                                        Icon(Icons.search),
-                                        Spacer(),
-                                        Icon(Icons.shopping_cart),
-                                        Spacer(),
-                                        Expanded(
-                                          flex: 2,
-                                          child: Container(color: Colors.black),
+                                        Container(
+                                          color: Colors.black,
+                                          child: ConstrainedBox(
+                                            constraints: BoxConstraints(
+                                              maxHeight: navHeight,
+                                              minHeight: navHeight * 0.6,
+                                              maxWidth: navHeight,
+                                              minWidth: navHeight * 0.6,
+                                            ),
+                                            child: Center(
+                                              child: Padding(
+                                                padding: EdgeInsets.all(
+                                                    navHeight * 0.12),
+                                                child: FittedBox(
+                                                  child: Image.network(
+                                                      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmIqX6p9xyGWcZ2rqEjQjYmsnlxy-XrhGtSD6xk4fksXcSOBcV'),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
                                         )
                                       ],
                                     ),
